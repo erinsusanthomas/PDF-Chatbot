86cf753d3609873e83d867850ba21bcc6434914c
Erin Susan Thomas <erinsusanthomas@gmail.com>
2024-10-02 12:15:21 +0300

    Revert code to use ChromaDB

--- RAG_code.py
+++ RAG_code.py
@@ -3,12 +3,19 @@ from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
 # from langchain_chroma import Chroma
-# from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.prompts import ChatPromptTemplate
 
-import faiss
-from langchain_community.vectorstores import FAISS
-from langchain_community.docstore.in_memory import InMemoryDocstore
+# import faiss
+# from langchain_community.vectorstores import FAISS
+# from langchain_community.docstore.in_memory import InMemoryDocstore
+# index = faiss.IndexFlatL2(1024)
+# db = FAISS(
+#     embedding_function=get_embedding_function(),
+#     index=index,
+#     docstore= InMemoryDocstore(),
+#     index_to_docstore_id={}
+# )
 
 ollama_model_type = "stablelm-zephyr" # reference:https://ollama.com/library/stablelm-zephyr
 ollama_embedding = 'nomic-embed-text' # reference: https://ollama.com/library/nomic-embed-text
@@ -44,37 +51,28 @@ def get_embedding_function():
     embeddings = OllamaEmbeddings(model=ollama_embedding)
     return embeddings
 
-index = faiss.IndexFlatL2(1024)
-db = FAISS(
-    embedding_function=get_embedding_function(),
-    index=index,
-    docstore= InMemoryDocstore(),
-    index_to_docstore_id={}
-)
-
 def add_to_chroma(store_dir, chunks: list[Document]):
-    # db = Chroma(persist_directory = store_dir, embedding_function=get_embedding_function())
+    db = Chroma(persist_directory = store_dir, embedding_function=get_embedding_function())
     chunks_with_ids = calculate_chunk_ids(chunks)
-    db.add_documents(chunks, ids=chunks_with_ids)
 
-    # # Add or Update documents
-    # existing_items = db.get(include=[])
-    # existing_ids = set(existing_items['ids'])
-    # print(f"No. of existing documents in DB: {len(existing_ids)}")
+    # Add or Update documents
+    existing_items = db.get(include=[])
+    existing_ids = set(existing_items['ids'])
+    print(f"No. of existing documents in DB: {len(existing_ids)}")
 
-    # # Only add documents that don't exist in DB
-    # new_chunks = []
-    # for chunk in chunks_with_ids:
-    #     if chunk.metadata['id'] not in existing_ids:
-    #         new_chunks.append(chunk)
-    # if len(new_chunks):
-    #     print(f"Adding new documents: {len(new_chunks)}")
-    #     new_chunk_ids = [chunk.metadata['id'] for chunk in new_chunks]
-    #     db.add_documents(new_chunks, ids=new_chunk_ids)
-    #     # db.persist()
-    #     print(f"All new documents added ✅")
-    # else:
-    #     print("No new documents to add ✅")
+    # Only add documents that don't exist in DB
+    new_chunks = []
+    for chunk in chunks_with_ids:
+        if chunk.metadata['id'] not in existing_ids:
+            new_chunks.append(chunk)
+    if len(new_chunks):
+        print(f"Adding new documents: {len(new_chunks)}")
+        new_chunk_ids = [chunk.metadata['id'] for chunk in new_chunks]
+        db.add_documents(new_chunks, ids=new_chunk_ids)
+        # db.persist()
+        print(f"All new documents added ✅")
+    else:
+        print("No new documents to add ✅")
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context: {context}
@@ -83,7 +81,7 @@ Answer the question based on the above context: {question}
 """
 
 def get_response(store_dir, query_text):
-    # db = Chroma(persist_directory=store_dir, embedding_function=get_embedding_function())
+    db = Chroma(persist_directory=store_dir, embedding_function=get_embedding_function())
     results = db.similarity_search_with_score(query_text, k=5)
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
 
