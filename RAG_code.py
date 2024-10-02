8e7aeadaaea1c10863017da0a3c946b61ff2c3fc
Erin Susan Thomas <erinsusanthomas@gmail.com>
2024-10-02 12:24:47 +0300

    Update to resolve chromdb issue on streamlit

--- RAG_code.py
+++ RAG_code.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3') 
+
 from langchain_ollama import OllamaLLM, OllamaEmbeddings
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
